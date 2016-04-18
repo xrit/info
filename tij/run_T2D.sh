7c4f105214c1000a79678b9e6f138295237abaa2
auser <xrit@advisical.com>
2016-04-18 15:12:33 -0700

    modified

--- tij/run_T2D.sh
+++ tij/run_T2D.sh
@@ -1,2 +1,8 @@
-java com.experimentalproj.T2D 0
+#! /usr/bin/env bash
+
+case $# in
+0) echo "$0 <time in ms>" ;;
+1) java com.experimentalproj.T2D $1 ;;
+*) echo "This shell script takes one argument only" ;;
+esac
 
