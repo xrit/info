bad6e3128121fb0c317664ab9098669f8b89e800
auser <xrit@advisical.com>
2016-12-01 21:18:01 -0800

    votes queries

--- directions/votesq.sql
+++ directions/votesq.sql
@@ -1,6 +1,8 @@
-select * from polls_choice c join polls_question q on (q.id = c.question_id);
+select * from polls_choice c join polls_question q on (q.id = c.question_id)
+order by c.question_id, c.id;
 
-select sum(c.votes), q.question_text, q.id 
+select sum(c.votes), q.id, q.question_text 
 from polls_choice c join polls_question q on (q.id = c.question_id) 
-group by q.question_text;
+group by q.id, q.question_text
+order by sum(c.votes);
 
