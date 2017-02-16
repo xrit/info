d48cf14404497014edb87da03828d71e3d2caa92
auser <xrit@advisical.com>
2017-02-15 16:26:23 -0800

    basic stuff

---
+++ pyex/template.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import sys
+
+def func(argf):
+  print argf
+ 
+def main(): 
+  if len(sys.argv) != 2:
+    print "Usage: python " + sys.argv[0] + " <argument> "
+    exit()
+  elif sys.argv[1] == '?':
+    print "Please input an argument."
+    argument = raw_input('Argument? ')
+  else: 
+    argument = sys.argv[1]
+  for i in range(0,10):
+    print "Your argument: ", argument, i
+  for t in list(argument):
+    print t
+  for s in range(3):
+    func(argument)
+
+if __name__ == '__main__':
+  main()
