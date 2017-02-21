d8644fbe7378d688bd1d0ab61dfed6a2c5517794
auser <xrit@advisical.com>
2017-02-21 10:28:34 -0800

    more hint for ...

--- pyex/LLD/LLDgpsmpro.py
+++ pyex/LLD/LLDgpsmpro.py
@@ -8,7 +8,11 @@ execfile("../dbcred.py")
 totaldist = -1
 cnx = mysql.connector.connect(user=username, password=passcode, host=hostname, database=dataname)
 idate = raw_input('UTC Date (YYYY-MM-DD) ? ')
-
 ...
-
-
+    dist = distanceFunc.calculate_distance_in_km(lati0, long0, lati1, long1)
+    totaldist = totaldist + dist
+    print dt_ms, "\t", dtime, "\t", 
+    print '{:+10.9f}'.format(lati0), "\t", '{:+10.9f}'.format(long0), "\t", 
+    print '{:+10.9f}'.format(lati1), "\t", '{:+10.9f}'.format(long1), "\t", 
+    print '{:20.9f}'.format(dist), "\t", totaldist
+...
