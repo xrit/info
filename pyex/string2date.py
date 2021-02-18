2345bbde5426cc9ade3124d00a4fdd09489374b6
xrit test <xtest@advisical.com>
2021-02-18 00:52:05 +0000

    string to date ex

---
+++ pyex/string2date.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import datetime
+import sys
+
+if (len(sys.argv) <> 2):
+   print("\n   Usage:   %s  <YYYY-MM-DD> \n" % sys.argv[0])
+   sys.exit(0)
+
+str_date = sys.argv[1]
+str_format = '%Y-%m-%d'
+
+istrd = datetime.datetime.strptime(str_date, str_format)
+nextd = istrd + datetime.timedelta(days=1)
+prevd = istrd + datetime.timedelta(days=-1)
+
+print(prevd)
+print(istrd)
+print(nextd)
+
