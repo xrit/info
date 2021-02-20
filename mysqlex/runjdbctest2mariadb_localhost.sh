451f03079ae921c2789ea29e877dc922a65cfefc
Alex Au <alexau@alexau>
2021-02-19 16:09:20 -0800

    changed run script for JDBC test

--- mysqlex/runjdbctest2mariadb_localhost.sh
+++ mysqlex/runjdbctest2mariadb_localhost.sh
@@ -1,2 +1,7 @@
 #! /usr/bin/env bash
-java -cp mysql-connector-java-5.1.10-bin.jar:. jdbctest2mariadb_localhost
+if [ "$1" == "" ]
+then
+	echo "usage: $0 <jdbc jar dir>"
+	exit 0
+fi
+java -cp $1/mysql-connector-java-5.1.10-bin.jar:. jdbctest2mariadb_localhost
