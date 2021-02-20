c420c93f1b27769abb1e2180a170b6fa741962e6
xrit test <xtest@advisical.com>
2021-02-20 19:57:12 +0000

    test psql jdbc

--- psqlex/runjdbctest2psqlrds.sh
+++ psqlex/runjdbctest2psqlrds.sh
@@ -1,2 +1,9 @@
 #! /usr/bin/env bash
-java -cp postgresql-9.3-1102.jdbc41.jar:. jdbctest2psqlrds
+
+if [ "$1" == "" ]
+then
+        echo "usage: $0 <test class/jdbc jar dir>"
+        exit 0
+fi
+cd $1
+java -cp .:postgresql-9.3-1102.jdbc41.jar jdbctest2psqlrds
