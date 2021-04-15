eaf51eddd6d77cf198d0cea395bb445a88dcb21f
xrit test <xtest@advisical.com>
2021-04-15 01:18:31 +0000

    test python3

--- pyex/template.py
+++ pyex/template.py
@@ -3,23 +3,27 @@
 import sys
 
 def func(argf):
-  print argf
+  print(argf)
  
 def main(): 
   if len(sys.argv) != 2:
-    print "Usage: python " + sys.argv[0] + " <argument> "
+    print("Usage: python " + sys.argv[0] + " <argument> ")
     exit()
   elif sys.argv[1] == '?':
-    print "Please input an argument."
-    argument = raw_input('Argument? ')
+    print("Please input an argument.")
+    if sys.version_info[0] < 3:
+      argument = raw_input('Argument? ')
+    else:
+      argument = input('Argument? ')
   else: 
     argument = sys.argv[1]
   for i in range(0,10):
-    print "Your argument: ", argument, i
+    sys.stdout.write("Your argument: %s %d\n" % (argument, i))
   for t in list(argument):
-    print t
+    print(t)
   for s in range(3):
     func(argument)
 
 if __name__ == '__main__':
   main()
+
